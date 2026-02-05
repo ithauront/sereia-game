410196b17683d59818e978f89e1b47e42ba08ce7
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-02-05 10:59:41 +0100

    refactor[components/character]: character component now consumes context for selected character

--- src/Components/Character/index.tsx
+++ src/Components/Character/index.tsx
@@ -1,56 +1,20 @@
-import playerBackLleg from '../../assets/playerTemplates/playerBackLleg.png'
-import playerBackRleg from '../../assets/playerTemplates/playerBackRleg.png'
-import playerFront from '../../assets/playerTemplates/playerFront.png'
-import playerFrontlLeg from '../../assets/playerTemplates/playerFrontLLeg.png'
-import playerFrontRleg from '../../assets/playerTemplates/playerFrontRleg.png'
-import playerLeft from '../../assets/playerTemplates/playerLeft.png'
-import playerLeftLleg from '../../assets/playerTemplates/playerLeftLleg.png'
-import playerLeftRleg from '../../assets/playerTemplates/playerLeftRleg.png'
-import playerRight from '../../assets/playerTemplates/playerRight.png'
-import playerRightLleg from '../../assets/playerTemplates/playerRightLleg.png'
-import playerRightRleg from '../../assets/playerTemplates/playerRightRleg.png'
-import type { CharDirection } from '../../hooks/useCharacterMovement'
+import { useCharacterContext } from '../../Contexts/characterContext'
+import type { CharDirection } from '../../hooks/useCharacterMovementInWorld'
 import { useWalinkAnimation } from '../../hooks/useWalkingAnimation'
 
-//TODO: update for different characters
-
 interface CharacterProps {
-  characterType?: string
   direction?: CharDirection
   isWalking: boolean
 }
 
-export function Character({
-  characterType = 'boy',
-  direction = 'front',
-  isWalking,
-}: CharacterProps) {
+export function Character({ direction = 'front', isWalking }: CharacterProps) {
+  const { selectedCharacter } = useCharacterContext()
   const { verticalFrameIndex, horizontalFrameIndex } = useWalinkAnimation({
     isWalking,
   })
-
-  let frames: string[] = []
-  let isVertical = true
-  if (characterType == 'boy') {
-    switch (direction) {
-      case 'front':
-        frames = [playerFront, playerFrontRleg, playerFrontlLeg]
-        isVertical = true
-        break
-      case 'back':
-        frames = [playerFront, playerBackRleg, playerBackLleg]
-        isVertical = true
-        break
-      case 'left':
-        frames = [playerLeft, playerLeftLleg, playerLeftRleg]
-        isVertical = false
-        break
-      case 'right':
-        frames = [playerRight, playerRightLleg, playerRightRleg]
-        isVertical = false
-        break
-    }
-  }
+  if (!selectedCharacter) return null
+  const frames = selectedCharacter?.sprites[direction]
+  const isVertical = direction === 'front' || direction === 'back'
 
   const frameIndex = isVertical ? verticalFrameIndex : horizontalFrameIndex
 
