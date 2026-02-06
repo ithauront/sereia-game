a76eb229cb94bb49b781c21cf24f107e3ad90b18
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-02-06 08:42:25 +0100

    feat[app]: added world and character provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 
+import { CharacterProvider } from './Contexts/characterContext'
+import { WorldProvider } from './Contexts/worldContext'
 import { AppRoutes } from './Routes/app.routes'
 
 export function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <WorldProvider>
+        <CharacterProvider>
+          <AppRoutes />
+        </CharacterProvider>
+      </WorldProvider>
     </BrowserRouter>
   )
 }
