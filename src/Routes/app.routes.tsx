27146df786eabda30ff6ebc58af2de82539d3d3e
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-04-01 08:45:43 +0200

    feat[src/routes/app.routes]: added all routes to router

--- src/Routes/app.routes.tsx
+++ src/Routes/app.routes.tsx
@@ -4,6 +4,10 @@ import { CharacterSelection } from '../Pages/CharacterSelect'
 import { NotFound } from '../Pages/NotFound'
 import { Start } from '../Pages/Start'
 import { RequireCharacterGuard } from './Guards/RequireCharacterGuard'
+import { MermaidInstructions } from '../Pages/MermaidInstruction'
+import { GameOver } from '../Pages/GameOver'
+import { Cutscene } from '../Pages/Cutscene'
+import { SereiaTattoo } from '../Pages/SereiaTattoo'
 
 export function AppRoutes() {
   return (
@@ -11,7 +15,11 @@ export function AppRoutes() {
       <Route path="/" element={<CharacterSelection />} />
 
       <Route element={<RequireCharacterGuard />}>
+        <Route path="/mermaid" element={<MermaidInstructions />} />
         <Route path="/start" element={<Start />} />
+        <Route path="/cutscene/:id" element={<Cutscene />} />
+        <Route path="/sereia-tattoo" element={<SereiaTattoo />} />
+        <Route path="/game-over" element={<GameOver />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
